ec68990e9ba444c9aa53218b3a4765efc4627548
Fat_Cat.dev <parelyzed123@outlook.com>
2025-01-21 20:14:06 -0500

    Add transparest bar

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const path = window.location.pathname;
+
+    if (path.includes("home")) {
+        document.getElementById("homeButton").classList.add("active");
+    } else if (path.includes("about")) {
+        document.getElementById("aboutButton").classList.add("active");
+    } else if (path.includes("contact")) {
+        document.getElementById("contactButton").classList.add("active");
+    }
+});
